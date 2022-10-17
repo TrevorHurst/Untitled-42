dac24251b57f639dc8010c6aa3cc5959a4d9a766
trevor-hurst <trevorshurst@gmail.com>
2022-10-17 18:08:56 -0500

    Working version

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask import request
 from flask import render_template
 
+messages = []
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def hello_world():
-    return render_template('main.html')
+    if request.method == "POST":
+        messages.append(request.form['message'])
+        return render_template('main.html',messagey = messages)
+    else:
+        return render_template('main.html',messagey = messages)
 
 
 if __name__ == "__main__":
